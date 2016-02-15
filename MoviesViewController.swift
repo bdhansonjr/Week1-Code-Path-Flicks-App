1509fdf97c0fbc604fd6f3f62d41ab56bad77e62
bhanson32 <bhanson@umbc.edu>
2016-02-14 23:51:45 -0500

    Week 1 & Week 2

--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -18,7 +18,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var NetworkError: UIView!
+    
+    
     var movies: [NSDictionary]?
+    var endpoint: String!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,8 +36,52 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Display HUD right before the request is made
         MBProgressHUD.showHUDAddedTo(self.view, animated: true)
         
-        // Hide HUD once the network request comes back (must be done on main UI thread)
-        MBProgressHUD.hideHUDForView(self.view, animated: true)
+        // Initialize Reachability
+        let reachability: Reachability
+        do {
+            reachability = try Reachability.reachabilityForInternetConnection()
+        } catch {
+            print("Unable to create Reachability")
+            return
+        }
+        
+        
+        // Reachability Conditions
+        reachability.whenReachable = { reachability in
+            // this is called on a background thread, but UI updates must
+            // be on the main thread, like this:
+            
+            self.NetworkError.hidden = true
+            
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                if reachability.isReachableViaWiFi() {
+                    print("Reachable via WiFi")
+                    
+                } else {
+                    print("Reachable via Cellular")
+                }
+            }
+        }
+        
+        reachability.whenUnreachable = { reachability in
+            // this is called on a background thread, but UI updates must
+            // be on the main thread, like this:
+            
+            self.NetworkError.hidden = false
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                print("Not reachable")
+            }
+        }
+        
+        do {
+            try reachability.startNotifier()
+        } catch {
+            print("Unable to start notifier")
+        }
+        
+        
         
         tableView.dataSource = self
         tableView.delegate = self
@@ -65,7 +114,67 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         task.resume()
         
         // Do any additional setup after loading the view.
+        
+        // Hide HUD once the network request comes back (must be done on main UI thread)
+        MBProgressHUD.hideHUDForView(self.view, animated: true)
+        
     }
+    
+    
+    
+//    func fadeIn() {
+//        
+//        NetworkError.alpha = 0;
+//        [ beginAnimations:nil context:nil];
+//        [UIViewController setAnimationCurve:UIViewAnimationCurveEaseIn];
+//        
+//        //don't forget to add delegate.....
+//        [UIView setAnimationDelegate:self];
+//        
+//        [UIView setAnimationDuration:2];
+//        NetworkError.alpha = 1;
+//        
+//        //also call this before commit animations......
+//        [UIView setAnimationDidStopSelector:@selector(animationDidStop:finished:context:)];
+//        [UIView commitAnimations];
+//        
+//        print("hello")
+//        
+//    }
+    
+    
+    
+    
+    func networkRequest() {
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string:"https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+    
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            print("response: \(responseDictionary)")
+                            
+                            self.movies = responseDictionary["results"] as! [NSDictionary]
+                            self.tableView.reloadData()
+                    }
+                }
+        });
+        task.resume()
+    }
+
+    func didRefresh(){
+        networkRequest()
+    }
+    
+    
     
     
     
@@ -88,9 +197,20 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         )
         
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(myRequest,
-            completionHandler: { (data, response, error) in
+            completionHandler: { (dataOrNil, response, error) in
                 
                 // ... Use the new data to update the data source ...
+                
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            print("response: \(responseDictionary)")
+                            
+                            self.movies = responseDictionary["results"] as! [NSDictionary]
+                            self.tableView.reloadData()
+                    }
+                }
+
                 
                 // Reload the tableView now that there is new data
                 self.tableView.reloadData()
@@ -121,16 +241,22 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
-        let posterPath = movie["poster_path"] as! String
-        
-        let baseUrl = "http://image.tmdb.org/t/p/w500"
-        let imageUrl = NSURL(string: baseUrl + posterPath)
-        
-        
-        
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-        cell.posterview.setImageWithURL(imageUrl!)
+        
+        let baseUrl = "http://image.tmdb.org/t/p/w500"
+        
+        if let posterPath = movie["poster_path"] as? String {
+            let imageUrl = NSURL(string: baseUrl + posterPath)
+            cell.posterview.setImageWithURL(imageUrl!)
+        }
+        
+        cell.accessoryType = .None
+        
+//        // Use a red color when the user selects the cell
+//        let backgroundView = UIView()
+//        backgroundView.backgroundColor = UIColor.redColor()
+//        cell.selectedBackgroundView = backgroundView
         
         
         print("row \(indexPath.row)")
@@ -139,14 +265,23 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
 
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPathForCell(cell)
+        let movie = movies![indexPath!.row]
+        
+        let detailViewController = segue.destinationViewController as! DetailsViewController
+        detailViewController.movie = movie
+        
+        
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
